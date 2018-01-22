2ff6080dbb3c7bd2e922eb98210660ab22b17e81
Weston Catron <weston@heatworks.tech>
2018-01-22 10:52:24 -0500

    Add chip select.

--- spi_read/read.c
+++ spi_read/read.c
@@ -7,6 +7,7 @@
 int xfer(int tx, int CLK_PIN, int MISO_PIN, int MOSI_PIN, int CS_PIN) {
     unsigned char t;  
     unsigned char shiftin=0; 
+    digitalWrite (CS_PIN, 1);
     digitalWrite (CLK_PIN, 0);
     for (t=0; t < 8; t++) { 
         if ((tx & 0x80) == 0) 
@@ -23,6 +24,7 @@ int xfer(int tx, int CLK_PIN, int MISO_PIN, int MOSI_PIN, int CS_PIN) {
             shiftin |= 1; 
         
     } 
+    digitalWrite (CS_PIN, 0);
     return shiftin;
 }
 
