7a7d90b2302357cbca6cd7acb0dd9bda8dd6f48d
Weston Catron <weston@heatworks.tech>
2018-01-22 13:37:09 -0500

    Use usleep, insetad of delay. Refactor.

--- spi_read/read.c
+++ spi_read/read.c
@@ -3,32 +3,60 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <unistd.h>
+
+#define CLK_DELAY 1000
 
 int xfer(int tx, int CLK_PIN, int MISO_PIN, int MOSI_PIN, int CS_PIN) {
     unsigned char t;  
     unsigned char shiftin=0; 
     digitalWrite (CLK_PIN, 0);
     digitalWrite (CS_PIN, 0);
-    delay(1);
+    usleep(CLK_DELAY);
     for (t=0; t < 8; t++) { 
         digitalWrite (CLK_PIN, 0);
         if ((tx & 0x80) == 0) 
             digitalWrite (MOSI_PIN, 0);
         else 
             digitalWrite (MOSI_PIN, 1);
-        delay(1);
+        usleep(CLK_DELAY);
         digitalWrite (CLK_PIN, 1);
         tx <<= 1;    //shift left one bit 
         shiftin <<= 1; //shift left one bit 
         if (digitalRead(MISO_PIN) == 1) 
             shiftin |= 1;
-        delay(1);
+        usleep(CLK_DELAY);
     } 
     digitalWrite (CLK_PIN, 0);
     digitalWrite (CS_PIN, 1);
     return shiftin;
 }
 
+double getTimestamp() {
+    struct timespec spec;
+    clock_gettime(CLOCK_REALTIME, &spec);
+    long            ms; // Milliseconds
+    time_t          s;  // Seconds
+    s  = spec.tv_sec;
+    ms = round(spec.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
+    if (ms > 999) {
+        s++;
+        ms = 0;
+    }
+    return (double)s + ((float)ms / 1000.0);
+}
+
+int readADCChannel(int channel, int CLK_PIN, int MISO_PIN, int MOSI_PIN, int CS_PIN) {
+    int response = xfer(0b10000001 + (channel << 4), CLK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
+    int responseA = xfer(0b0, CLK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
+    int responseB = xfer(0b0, CLK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
+    int responseC = xfer(0b0, CLK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
+
+    int left = responseC >> 7;
+    int middle = responseB << 1;
+    int right = responseA << 9;
+    return left + middle + right;
+}
 
 int main (int argc, char *argv[])
 {
@@ -51,34 +79,11 @@ int main (int argc, char *argv[])
     pinMode (SPI_MOSI, OUTPUT) ;
     pinMode (SPI_CS, OUTPUT) ;
 
-    // Get timestamp.
-    struct timespec spec;
-    clock_gettime(CLOCK_REALTIME, &spec);
-    long            ms; // Milliseconds
-    time_t          s;  // Seconds
-    s  = spec.tv_sec;
-    ms = round(spec.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
-    if (ms > 999) {
-        s++;
-        ms = 0;
-    }
-    float value = 5.0;
-
-    int response = xfer(0b10000001 + (CHANNEL << 4), SPI_CLK, SPI_MISO, SPI_MOSI, SPI_CS);
-    printf("%i -> %i\n",0b10000001 + (CHANNEL << 4), response);
-    int responseA = xfer(0b0, SPI_CLK, SPI_MISO, SPI_MOSI, SPI_CS);
-    printf("%i -> %i\n",0b0, responseA);
-    int responseB = xfer(0b0, SPI_CLK, SPI_MISO, SPI_MOSI, SPI_CS);
-    printf("%i -> %i\n",0b0, responseB);
-    int responseC = xfer(0b0, SPI_CLK, SPI_MISO, SPI_MOSI, SPI_CS);
-    printf("%i -> %i\n",0b0, responseC);
-
-    int left = responseC >> 7;
-    int middle = responseB << 1;
-    int right = responseA << 9;
-    int final = left + middle + right;
-
     // Print results.
-    printf("%i.%03ld,%i,%i\n", (int)s, ms, CHANNEL, final); 
+    while(1) {
+        printf("%.3f,%i,%i\n", getTimestamp(), CHANNEL, readADCChannel(CHANNEL,SPI_CLK, SPI_MISO, SPI_MOSI, SPI_CS)); 
+        usleep(1000000);
+    }
+   
     return 0 ;
 }
